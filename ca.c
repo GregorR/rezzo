a86a5bfa288243f8dd458b47e46e815210b0190a
Gregor Richards <Richards@codu.org>
2011-08-22 16:05:18 -0400

    I implemented the CA wrong HOORAY >_>

--- ca.c
+++ ca.c
@@ -101,11 +101,10 @@ void randWorld(World *world)
     for (d = 0; d < tod; d++) {
         i = getCell(world, x, y);
         if ((dx == 0 && dy == 0) || world->c[i] != CELL_NONE) {
-            world->c[i] = CELL_BASE;
             x = random() % (w/4) * 4;
             y = random() % (h/4) * 4;
             dx = dy = 0;
-            while ((!dx && !dy) || (dx && dy)) {
+            while (!dx && !dy) {
                 dx = (random() % 3) - 1;
                 dy = (random() % 3) - 1;
             }
@@ -199,10 +198,11 @@ void updateCell(World *world, int x, int y, unsigned char *c, unsigned char *own
 
     if (self == CELL_CONDUCTOR) {
         /* check for electrons in the neighborhood */
+        unsigned char electrons = 0;
         for (i = 0; i < 9; i++) {
-            if (ncs[i] == CELL_ELECTRON) break;
+            if (ncs[i] == CELL_ELECTRON) electrons++;
         }
-        if (i < 9)
+        if (electrons == 1 || electrons == 2)
             *c = CELL_ELECTRON;
 
     } else if (self == CELL_ELECTRON) {
